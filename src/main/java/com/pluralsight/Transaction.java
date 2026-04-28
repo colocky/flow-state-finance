e081f2845392a69fb2d1fff5ecd52bc6f3eb41a0
colocky <callmeadam.ar@gmail.com>
2026-04-28 10:59:28 -0500

    Completed: Transaction - object-class.

---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,66 @@
+package com.pluralsight;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Transaction {
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    ////////// CONSTRUCTOR //////////
+    public Transaction(LocalDate date, LocalTime time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+    ////////// GETTERS AND SETTERS //////////
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    ////////// TRANSACTION METHODS //////////
+    public boolean isDeposit() {
+        return amount > 0;
+    }
+
+    public boolean isPayment() {
+        return amount < 0;
+    }
+    ////////// TO CSV FORMAT //////////
+    public String toCsvLine() {
+        return date + "|" + time + "|" + description + "|" + vendor + "|" + amount;
+    }
+
+    ////////// TO STRING FORMAT //////////
+    public String toString() {
+        return String.format(
+                "%s | %s | %s | %s | $%.2f",
+                date,
+                time,
+                description,
+                vendor,
+                amount
+        );
+    }
+}
